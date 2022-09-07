93549061cfe15085beec6c7ff82a4bf2249f3163
Adrien Guillo <adrien.guillo@gmail.com>
2022-09-07 15:34:56 +0200

    Add fish functions

---
+++ fish/functions/aws2.fish
@@ -0,0 +1,3 @@
+function aws2 --wraps=/usr/local/bin/aws --description 'alias aws2=/usr/local/bin/aws'
+  /usr/local/bin/aws $argv; 
+end
---
+++ fish/functions/la.fish
@@ -0,0 +1,3 @@
+function la --wraps=ls --wraps='ls -la' --description 'alias la=ls -la'
+  ls -la $argv; 
+end
